80a6607e326f1e6357d0e910bc6fd5ae7ac88771
sun-3 <72216343+sun-3@users.noreply.github.com>
2022-08-17 11:50:07 +0530

    Create Fast.java

---
+++ src/recursion/Fast.java
@@ -0,0 +1,59 @@
+package util;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Fast {
+    public static void main(String[] args){
+        FastScanner sc = new FastScanner();
+        int a = sc.nextInt();
+        long b = sc.nextLong();
+        double c = sc.nextDouble();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+    public static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner(String s) {
+            try {
+                br = new BufferedReader(new FileReader(s));
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        public FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String nextToken() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(nextToken());
+        }
+
+        long nextLong() {
+            return Long.parseLong(nextToken());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(nextToken());
+        }
+    }
+}
